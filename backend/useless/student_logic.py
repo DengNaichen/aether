6035aecbccbf71874084aacb498465ef8b9ab85b
DengNaiChen <dengnaichen@gmail.com>
2025-10-04 16:50:50 -0400

    delete some legacy files

--- backend/useless/student_logic.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.ext.declarative import declarative_base
-from passlib.context import CryptContext
-
-
-DATABASE_URL = "postgresql+psycopg2://learning_user:d1997225@localhost/learning_graph_db"
